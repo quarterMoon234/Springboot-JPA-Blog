6af30b0bf000f14e443945cbda1efef38f068c8d
quarterMoon234 <quarterMoon234@gmail.com>
2023-09-09 21:23:07 +0900

    commit message

---
+++ src/main/java/com/example/demo/test/BlogControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.test;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class BlogControllerTest {
+	
+	@GetMapping("/test/hello")
+	public String Hello() {
+	return "<h1>Hello Spring Boot</h1>";
+	}
+
+}
